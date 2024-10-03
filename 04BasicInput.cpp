612c20e4945df876fbdf5a359b9179a0b5fa7dfc
CLARISSA J.  CALAYAN <clarissacalayan6@gmail.com>
2024-10-03 11:54:28 +0800

    Update 04BasicInput.cpp

--- 04BasicInput.cpp
+++ 04BasicInput.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 int main()
 {
-string nickname, school, cyl;
+string nickname, school, course;
 cout<<"Nickname:";
 cin>>nickname;
 cout<<"Course-Year Level:";
-cin>>cyl;
+cin>>course;
 cout<<"School:";
 cin>>school;
+cout << "Wow, Congrats " << nickname << "!" << course << " is a nice course. And you are studying in " << school << ", which is one of the Centers of Excellence in Tertiary Education." << endl;
 return 0;
 }
