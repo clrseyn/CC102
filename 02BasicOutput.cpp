f2f16c9f6aa02266aa704e1e6155abef4f1d4587
CLARISSA J.  CALAYAN <clarissacalayan6@gmail.com>
2024-10-03 10:25:54 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{ 
+cout <<"Our Lady of Lourdes College Foundation"<<endl;
+cout <<"College of Computer Studies"<<endl;
+cout <<"Bachelor of Science in Information Technology-1st Year"<<endl;
+cout <<"Clarissa Juanir Calayan"<<endl;
+cout <<"Purok-3, Batobalani, Paracale, Camarines Norte"<<endl;
+return 0;
+}
